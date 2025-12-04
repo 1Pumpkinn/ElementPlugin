5761ef8d3a6cbc2c7bce51adc601384d8f5cec9c
Pumpkin <wishsmp1002@gmail.com>
2025-12-05 06:37:43 +1100

    Frost New Abilities

--- src/main/java/hs/elementPlugin/elements/abilities/impl/frost/FrostNovaAbility.java
+++ src/main/java/hs/elementPlugin/elements/abilities/impl/frost/FrostNovaAbility.java
@@ -5,10 +5,8 @@ import hs.elementPlugin.elements.abilities.BaseAbility;
 import hs.elementPlugin.elements.ElementContext;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
-import org.bukkit.block.Block;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Mob;
 import org.bukkit.entity.Player;
@@ -17,12 +15,8 @@ import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class FrostNovaAbility extends BaseAbility {
     private final ElementPlugin plugin;
-    private final Set<Location> frozenBlocks = new HashSet<>();
 
     public static final String META_NOVA_FROZEN = "frost_nova_frozen";
 
@@ -41,7 +35,7 @@ public class FrostNovaAbility extends BaseAbility {
         player.getWorld().playSound(center, Sound.BLOCK_GLASS_BREAK, 2.0f, 0.5f);
         player.getWorld().playSound(center, Sound.ENTITY_GENERIC_EXPLODE, 1.0f, 1.5f);
 
-        // Create expanding ice explosion effect
+        // Create expanding ice spike/burst pattern (different from air blast)
         new BukkitRunnable() {
             double currentRadius = 0;
             int ticks = 0;
@@ -53,26 +47,47 @@ public class FrostNovaAbility extends BaseAbility {
                     return;
                 }
 
-                // Spawn particle ring
-                for (int i = 0; i < 360; i += 10) {
+                // Create multiple expanding spike rings at different heights
+                for (double height = 0; height <= 1.5; height += 0.5) {
+                    // Outer ring of ice spikes
+                    for (int i = 0; i < 360; i += 8) {
+                        double rad = Math.toRadians(i);
+                        double x = Math.cos(rad) * currentRadius;
+                        double z = Math.sin(rad) * currentRadius;
+
+                        Location particleLoc = center.clone().add(x, height, z);
+
+                        // Dense ice/snow particles forming spikes
+                        player.getWorld().spawnParticle(Particle.SNOWFLAKE, particleLoc, 5, 0.1, 0.2, 0.1, 0, null, true);
+                        player.getWorld().spawnParticle(Particle.FIREWORK, particleLoc, 2, 0.05, 0.1, 0.05, 0.02, null, true);
+                    }
+                }
+
+                // Inner spiral of ice crystals
+                for (int i = 0; i < 8; i++) {
+                    double angle = Math.toRadians(i * 45 + ticks * 30);
+                    double spiralRadius = currentRadius * 0.6;
+                    double x = Math.cos(angle) * spiralRadius;
+                    double z = Math.sin(angle) * spiralRadius;
+
+                    Location spiralLoc = center.clone().add(x, 0.5, z);
+                    player.getWorld().spawnParticle(Particle.SNOWFLAKE, spiralLoc, 8, 0.2, 0.3, 0.2, 0.05, null, true);
+                }
+
+                // Ground frost effect
+                for (int i = 0; i < 360; i += 15) {
                     double rad = Math.toRadians(i);
                     double x = Math.cos(rad) * currentRadius;
                     double z = Math.sin(rad) * currentRadius;
 
-                    Location particleLoc = center.clone().add(x, 0.1, z);
-
-                    // Ice particles
-                    player.getWorld().spawnParticle(Particle.SNOWFLAKE, particleLoc, 3, 0.1, 0.2, 0.1, 0, null, true);
-                    player.getWorld().spawnParticle(Particle.CLOUD, particleLoc, 1, 0.1, 0.1, 0.1, 0, null, true);
+                    Location groundLoc = center.clone().add(x, 0.05, z);
+                    player.getWorld().spawnParticle(Particle.CLOUD, groundLoc, 2, 0.2, 0.05, 0.2, 0, null, true);
                 }
 
                 currentRadius += 0.5;
                 ticks++;
             }
         }.runTaskTimer(plugin, 0L, 1L);
-
-        // Freeze ground in radius
-        freezeGround(center, radius);
 
         // Freeze nearby enemies
         for (LivingEntity entity : center.getNearbyLivingEntities(radius)) {
@@ -88,65 +103,14 @@ public class FrostNovaAbility extends BaseAbility {
             // Apply freeze effect
             freezeEntity(entity, 5); // 5 seconds freeze
 
-            // Visual feedback
+            // Visual feedback - ice crystals bursting from entity
             entity.getWorld().spawnParticle(Particle.SNOWFLAKE, entity.getLocation().add(0, 1, 0),
-                    20, 0.3, 0.5, 0.3, 0, null, true);
+                    30, 0.4, 0.6, 0.4, 0.05, null, true);
+            entity.getWorld().spawnParticle(Particle.FIREWORK, entity.getLocation().add(0, 1, 0),
+                    15, 0.3, 0.5, 0.3, 0.02, null, true);
         }
 
         return true;
-    }
-
-    /**
-     * Freeze the ground in a radius, creating ice blocks temporarily
-     */
-    private void freezeGround(Location center, double radius) {
-        Set<Location> blocksToFreeze = new HashSet<>();
-
-        // Find all valid blocks to freeze
-        for (int x = (int) -radius; x <= radius; x++) {
-            for (int z = (int) -radius; z <= radius; z++) {
-                Location checkLoc = center.clone().add(x, 0, z);
-
-                // Check if within circular radius
-                if (checkLoc.distance(center) > radius) continue;
-
-                // Find the top solid block
-                Block block = checkLoc.getBlock();
-                while (block.getY() > center.getY() - 5 && block.getType().isAir()) {
-                    block = block.getRelative(0, -1, 0);
-                }
-
-                // Check if we can place ice on top
-                Block iceBlock = block.getRelative(0, 1, 0);
-                if (iceBlock.getType().isAir() && block.getType().isSolid()) {
-                    blocksToFreeze.add(iceBlock.getLocation());
-                }
-            }
-        }
-
-        // Freeze the blocks
-        for (Location loc : blocksToFreeze) {
-            Block block = loc.getBlock();
-            block.setType(Material.ICE);
-            frozenBlocks.add(loc);
-        }
-
-        // Schedule ice removal after 10 seconds
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                for (Location loc : blocksToFreeze) {
-                    Block block = loc.getBlock();
-                    if (block.getType() == Material.ICE) {
-                        block.setType(Material.AIR);
-                        // Small particle effect when ice melts
-                        block.getWorld().spawnParticle(Particle.CLOUD, loc.add(0.5, 0.5, 0.5),
-                                5, 0.3, 0.3, 0.3, 0, null, true);
-                    }
-                }
-                frozenBlocks.removeAll(blocksToFreeze);
-            }
-        }.runTaskLater(plugin, 200L); // 10 seconds
     }
 
     /**
